008b240297f3d5fd8736656c6693936b489bad6e
Delyan Angelov <delian66@gmail.com>
2025-10-31 15:16:37 +0200

    run `v fmt -w .`

--- main.v
+++ main.v
@@ -5,8 +5,10 @@ module main
 
 @[c_extern]
 __global C._binary_Inconsolata_16r_psf_start u8
+
 @[c_extern]
 __global C._binary_Inconsolata_16r_psf_end u8
+
 @[c_extern]
 __global C._binary_Inconsolata_16r_psf_size u8
 
@@ -27,17 +29,21 @@ fn main() {
 	// println(font.magic.hex()); println(font)
 	fg, bg := `*`, ` `
 	c := rune(arguments()[1] or { 'a' }[0])
-	mut glyph := unsafe { &C._binary_Inconsolata_16r_psf_start + font.headersize +
-	if c > 0 && c < font.numglyph { c } else { 0 } * font.bytesperglyph }
-    bytesperline := (font.width+7)/8
+	mut glyph := unsafe {
+		&C._binary_Inconsolata_16r_psf_start + font.headersize +
+			if c > 0 && c < font.numglyph { c } else { 0 } * font.bytesperglyph
+	}
+	bytesperline := (font.width + 7) / 8
 	for y := 0; y < font.height; y++ {
-	    mut mask := u32(1) << (font.width - 1)
+		mut mask := u32(1) << (font.width - 1)
 		for x := 0; x < font.width; x++ {
 			v := if unsafe { *(&u32(glyph)) & mask } != 0 { fg } else { bg }
-		    mask = mask >> 1
+			mask = mask >> 1
 			print('${v} ')
 		}
-	    unsafe { glyph += bytesperline }
+		unsafe {
+			glyph += bytesperline
+		}
 		println('')
 	}
 }
